d27bd841b6568e13d64a0f48e3dfc7940a29e117
MSI <perun042199@gmail.com>
2017-11-09 12:58:35 +1000

    Ctrl+Z Practice

--- Practice/Practice.php
+++ Practice/Practice.php
@@ -1,17 +1,4 @@
 <?php
 class Practice
 {
-    private $student;
-    private $idToSend;
-    private $deadLine;
-    private $text;
-    private $arrayOfApplications;
-    private $cluster;
-
-    public function addNewPractice($cluster ,$text ,$deadLine , $idToSend){
-        $this->cluster = $cluster;
-        $this->text = $text;
-        $this->deadLine = $deadLine;
-        $this->idToSend = $idToSend;
-    }
 }
