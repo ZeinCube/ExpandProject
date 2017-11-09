4f2899148677aebad9395fc6f76a05df4b8e6ea7
Yury Kurlykov <tarakanama@mail.ru>
2017-11-10 00:48:08 +1000

    try to fix crash on vds

--- includes.php
+++ includes.php
@@ -1,9 +1,9 @@
 <?php
-include_once 'DB/Connection.php';
-include_once 'Cluster/Cluster.php';
-include_once 'Users/User.php';
-include_once 'Users/Auth.php';
-include_once 'Task/Task.php';
-include_once 'Task/Solution.php';
-include_once 'Practice/Practice.php';
-include_once 'Practice/Application.php';
+include_once './DB/Connection.php';
+include_once './Cluster/Cluster.php';
+include_once './Users/User.php';
+include_once './Users/Auth.php';
+include_once './Task/Task.php';
+include_once './Task/Solution.php';
+include_once './Practice/Practice.php';
+include_once './Practice/Application.php';
